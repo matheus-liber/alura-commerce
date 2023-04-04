d693e2d187a8149b39b7c6c1142a33628bcc9f1e
matheus-liber <matheuslibergt@gmail.com>
2023-04-04 18:16:32 -0300

    Módulo1 completo

---
+++ src/telas/Login/index.js
@@ -0,0 +1,45 @@
+import { useState, useContext } from "react";
+import { Text, View, TextInput, TouchableOpacity, StatusBar } from 'react-native';
+import { estilo } from './estilos';
+import { GlobalContext } from "../../contexts/GlobalContext";
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const { valor } = useContext(GlobalContext)
+
+  return (
+    <View style={estilo.container}>
+      <StatusBar />
+      <Text style={estilo.titulo}>Login = {valor}</Text>
+
+      <View style={estilo.inputArea}>
+        <TextInput
+          style={estilo.input}
+          placeholder="Email"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={estilo.input}
+          placeholder="Senha"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          value={senha}
+          onChangeText={setSenha}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={estilo.botao}
+        onPress={() => navigation.navigate('Principal')}
+      >
+        <Text style={estilo.botaoTexto}>Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
