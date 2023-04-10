908935b13fb62d50ff347b35acac5453f6e35ce6
matheus-liber <matheuslibergt@gmail.com>
2023-04-10 14:01:19 -0300

    Módulo 5 finalizado

--- src/contexts/TemaContext.js
+++ src/contexts/TemaContext.js
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { escuro, claro } from "../estilosGlobais";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const TemaContext = createContext({})
 
@@ -10,12 +11,25 @@ export function TemaProvider( {children} ) {
         'escuro': escuro,
         'claro': claro
     }
+
+    useEffect( async () => {
+        const temaSalvo = await AsyncStorage.getItem('@tema');
+        if (temaSalvo) {
+            setTemaAtual(temaSalvo)
+        }
+    }, [])
+
+    async function salvarTemaNoDispositivo(tema) {
+        await AsyncStorage.setItem('@tema', tema)
+        setTemaAtual(tema)
+    }
     
     return (
         <TemaContext.Provider value={{
             temaAtual,
             setTemaAtual,
-            temaEscolhido: temas[temaAtual]
+            temaEscolhido: temas[temaAtual],
+            salvarTemaNoDispositivo
         }}>
             {children}
         </TemaContext.Provider>
